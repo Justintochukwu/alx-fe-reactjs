9fe79b8a797cbf7777e3ce276ad5758a809c588c
Justintochukwu <justintochukwu00@gmail.com>
2025-09-04 06:05:32 +0100

    styling

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+function MainContent() {
+  return (
+    <main style={{ padding: '20px', backgroundColor: '#eaf4ff', textAlign: 'center' }}>
+      <p style={{ fontSize: '18px', color: '#333' }}>
+        I love to visit New York, Paris, and Tokyo.
+      </p>
+    </main>
+  );
+}
+
+export default MainContent;
