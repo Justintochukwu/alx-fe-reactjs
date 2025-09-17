35ce1d6cce38a31bf74d7ad63c536c6f9c318d42
Justintochukwu <justintochukwu00@gmail.com>
2025-09-17 19:46:26 +0100

    addrecipe

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -4,6 +4,9 @@ import About from "./components/About";
 import Services from "./components/Services";
 import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
+import AddRecipeForm from "./components/Navbar";
+import RecipeList from "./components/Navbar";
+
 
 function App() {
   return (
@@ -15,6 +18,8 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/addrecipeform" element={<AddRecipeForm />} />
+          <Route path="/recipelist" element={<RecipeList />} />
         </Routes>
       </div>
     </Router>
