e76504bfb7b77e2da1f6756f3ab3d4b691b984cb
Justintochukwu <justintochukwu00@gmail.com>
2025-10-09 22:11:45 +0100

    route

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,12 +1,18 @@
 import { Navigate } from "react-router-dom";
 
-// ✅ Simple simulated authentication check
-const isAuthenticated = true; // change to false to test
+// ✅ Simulated authentication hook
+function useAuth() {
+  const user = { loggedIn: true }; // change to false to simulate not logged in
+  return user && user.loggedIn;
+}
 
 const ProtectedRoute = ({ children }) => {
+  const isAuthenticated = useAuth();
+
   if (!isAuthenticated) {
     return <Navigate to="/" replace />;
   }
+
   return children;
 };
 
