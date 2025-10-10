4f4f7d554afe626a40fc03f2102a39899f6abc6c
Justintochukwu <justintochukwu00@gmail.com>
2025-10-10 19:53:29 +0100

    todo

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import React from "react";
-import TodoList from "./components/TodoList"; // ✅ Import TodoList
+import TodoList from "../components/TodoList"; // ✅ Import TodoList
 
 function App() {
   return (
