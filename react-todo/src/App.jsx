58bf80f5931836b3ee5f8456a7b903ad2edfd8e4
Justintochukwu <justintochukwu00@gmail.com>
2025-10-10 21:36:26 +0100

    totdolist

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,12 +1,14 @@
 // src/App.jsx
 import React from "react";
 import TodoList from './components/TodoList';
+import AddTodoForm from './components/AddTodoForm';
 
 
 function App() {
   return (
     <div>
       <TodoList />
+      <AddTodoForm />
       <h1>Todo App</h1>
     </div>
   );
